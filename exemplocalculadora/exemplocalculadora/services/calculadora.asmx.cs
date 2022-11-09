79776b8810c8134dce0f7d16b8f93309c6b8c240
JoelFigueiras <joelfigueiras@live.com.pt>
2022-11-09 19:11:24 +0000

    first

---
+++ exemplocalculadora/exemplocalculadora/services/calculadora.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace exemplocalculadora.services
+{
+    /// <summary>
+    /// Descrição resumida de calculadora
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que esse serviço da web seja chamado a partir do script, usando ASP.NET AJAX, remova os comentários da linha a seguir. 
+    // [System.Web.Script.Services.ScriptService]
+    public class calculadora : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Olá, Mundo";
+        }
+
+        [WebMethod]
+        public int CalcularSoma(int x, int y)
+        {
+            System.Threading.Thread.Sleep(10000);
+            return x + y;
+        }
+    }
+}
